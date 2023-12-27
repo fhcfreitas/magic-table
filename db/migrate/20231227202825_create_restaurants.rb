3be1ff2fec19aaa717daf4d6c11dd0d8723fe7b1
Fabio Freitas <fhcfreitas@gmail.com>
2023-12-27 18:14:46 -0300

    restaurant mvc

---
+++ db/migrate/20231227202825_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration[7.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :location
+      t.string :address
+      t.string :category
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
